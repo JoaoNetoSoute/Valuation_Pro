f45b79e0c002a03b57cad77f3e9be436c0b901a0
JoaoNetoSoute <joao.neto@ifpi.edu.br>
2025-05-22 22:15:42 -0300

    Update comparables.py

--- modules/comparables.py
+++ modules/comparables.py
@@ -1,1 +1,8 @@
+def valuation_por_multiplos(preco_lucro, lucro_liquido):
+    """
+    Calcula o valor justo baseado em múltiplo P/L e lucro líquido anual.
+    """
+    if preco_lucro <= 0:
+        return None
+    return preco_lucro * lucro_liquido
 
